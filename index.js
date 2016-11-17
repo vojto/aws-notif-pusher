c43e491fe61db09a68680fc7bc4292a074bcbffc
Vojtech Rinik <vojto@rinik.net>
2016-11-17 14:07:57 +0100

    Implementing

--- index.js
+++ index.js
@@ -1,17 +1,76 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var AWS = require('aws-sdk');
+var async = require('async');
+
+AWS.config.update({ region:'eu-central-1' });
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.send('ok');
 });
+
+app.post('/push', function(request, response) {
+  var sns = new AWS.SNS();
+
+  var endpoints = request.body.endpoints.map(function(endpoint) {
+    return endpoint.replace(/\s/, '');
+  });
+
+  if (!endpoints || endpoints.length < 0) {
+    return response.send('missing argument endpoints');
+  }
+
+  var format = request.body.format;
+  if (!format) {
+    return response.send('missing argument format');
+  }
+
+  var notifBody = request.body.body;
+  if (!notifBody) {
+    return response.send('missing argument body');
+  }
+
+  async.each(endpoints, function(endpoint, next) {
+    var message = {};
+
+    message[format] = JSON.stringify(notifBody);
+
+    var params = {
+      Message: JSON.stringify(message),
+      'MessageStructure': 'json',
+      TargetArn: endpoint
+    };
+
+    console.log('SNS.Publish:');
+    console.log(params);
+    console.log('---');
+
+    sns.publish(params, function(err, data) {
+      next(err);
+    });
+  }, function(err) {
+    if (err) {
+      console.log(err);
+      response.send(err);
+    } else {
+      response.send('ok');
+    }
+
+  });
+
+  
+
+  
+
+});
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
